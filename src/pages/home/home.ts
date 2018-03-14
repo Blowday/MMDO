ce0489f41288f38f0d1bbc7583df5d8e6124b3cd
Blow <biow@hotmail.fr>
2018-03-14 16:02:23 +0100

    question 9e environ (aucun resultat marche pas)

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+export interface Result {
+  title : string;
+  author : string;
+  image : string;
+
+}
+
+const fakeResults : Result[] = [
+  {
+    title : 'Charlie et la chocolaterie', 
+    author : 'Charlie',
+    image : "http://img.over-blog-kiwi.com/0/93/14/92/20140323/ob_7bcc55_540792-394996087251191-1015015309-n.jpg"
+  },
+  {
+    title : 'Charlie et la carosserie', 
+    author : 'encore Charlie',
+    image : "http://img.over-blog-kiwi.com/0/93/14/92/20140323/ob_7bcc55_540792-394996087251191-1015015309-n.jpg"
+  },
+  {
+    title : 'Charlie et la bijouterie', 
+    author : 'sharly',
+    image : "http://img.over-blog-kiwi.com/0/93/14/92/20140323/ob_7bcc55_540792-394996087251191-1015015309-n.jpg"
+  }
+]
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  results: Result[];
 
   constructor(public navCtrl: NavController) {
-
+    //this.results = fakeResults;
   }
 
+  getItems(ev: any) {
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+    val ? this.results = fakeResults : this.results = [];
+  }
 }
+
